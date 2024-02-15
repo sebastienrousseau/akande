a9e11b90896149af79d3ebe9c78bb842b3f1ec5b
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2024-02-15 00:31:03 +0000

    fix(akande): :bug: Add conditional dependencies

--- setup.py
+++ setup.py
@@ -1,5 +1,21 @@
+# Copyright (C) 2024 Sebastien Rousseau.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 from pathlib import Path
 from setuptools import setup, find_packages
+import sys
 
 # Define the directory where setup.py is located
 this_directory = Path(__file__).parent
@@ -9,6 +25,13 @@ long_description = (this_directory / "README.md").read_text()
 requirements_path = this_directory / "requirements.txt"
 with open(requirements_path, "r") as file:
     requirements = file.read().splitlines()
+
+# Conditional dependency
+conditional_requirements = []
+if sys.platform == 'darwin':
+    conditional_requirements.extend([
+        'pyobjc-core==9.0.1',
+    ])
 
 setup(
     author="Sebastian Rousseau",
@@ -38,3 +61,4 @@ setup(
         "Programming Language :: Python :: 3.10",
     ],
 )
+
